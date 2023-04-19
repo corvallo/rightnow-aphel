484671b5030032c9e8f1bb568f96bc62b7f924f2
f.stallo <f.stallo@logispin.com>
2023-04-19 11:10:58 +0200

    index page, robot status

--- src/js/app.js
+++ src/js/app.js
@@ -6,11 +6,5 @@ import "./modules/sidebar";
 import "./modules/theme";
 import "./modules/feather";
 
-// Charts
-import "./modules/chartjs";
-
 // Forms
 import "./modules/flatpickr";
-
-// Maps
-import "./modules/vector-maps";
