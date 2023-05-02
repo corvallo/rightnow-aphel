0b2b4a7ef9cf29fbe5007b238331377dc4aa315f
f.stallo <f.stallo@logispin.com>
2023-05-02 17:31:47 +0200

    robot detail 2

--- src/js/app.js
+++ src/js/app.js
@@ -12,10 +12,33 @@ import "./users";
 import "./locations";
 import "./activities";
 import "./screening";
+import Choices from "choices.js";
 import $ from "jquery";
 import DataTable from "datatables.net-responsive";
 window.$ = $;
 $(document).ready(() => {
+  new Choices("#choices-places", { itemSelectText: "" });
+  $("#volumeRange")
+    .off()
+    .on("input", (e) => {
+      const value = e.currentTarget.value;
+      $("#volumeValue").html(`${value}%`);
+    })
+    .on("change", (e) => {
+      const value = e.currentTarget.value;
+      $("#volumeValue").html(`${value}%`);
+    });
+  $("#velocityRange")
+    .off()
+    .on("input", (e) => {
+      const value = e.currentTarget.value;
+      $("#velocityValue").html(`${value}%`);
+    })
+    .on("change", (e) => {
+      const value = e.currentTarget.value;
+      $("#velocityValue").html(`${value}%`);
+    });
+
   $("#activities-widget").DataTable({
     responsive: true,
     paging: false,
