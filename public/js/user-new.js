805d3822136279815c97078dd066a5e03abef2ae
f.stallo <f.stallo@logispin.com>
2023-05-06 14:12:24 +0200

    fix selects

--- public/js/user-new.js
+++ public/js/user-new.js
@@ -2,5 +2,13 @@ $(document).ready(() => {
   new Choices("#choices-title", { itemSelectText: "" });
   new Choices("#choices-locations", { itemSelectText: "" });
   new Choices("#choices-role", { itemSelectText: "" });
-  new Choices("#choices-lab", { itemSelectText: "" });
+  new Choices("#choices-lab", {
+    itemSelectText: "",
+    removeItemButton: true,
+    noResultsText: "Nessun risultato",
+    noChoicesText: "Nessun valore tra cui scegliere",
+    itemSelectText: "Seleziona",
+    uniqueItemText: "Possono essere aggiunti solo valori univi",
+    customAddItemText: "Possono essere aggiunti solo valori che soddisfano parametri specifici",
+  });
 });
